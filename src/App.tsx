e6ae043f5fa6b8ff6b551d361baef9ffef010d86
Vinicius <viniholtman28@gmail.com>
2022-10-31 08:52:13 -0300

    Create context to transactions

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components";
+import { TransactionProvider } from "./contexts/TransactionsContext";
 import { Transactions } from "./pages/Transactions";
 import { GlobalStyles } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
@@ -9,7 +10,9 @@ export function App() {
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
 
-      <Transactions />
+      <TransactionProvider>
+        <Transactions />
+      </TransactionProvider>
     </ThemeProvider>
   )
 }
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,8 +1,13 @@
 import { SummaryCard, SummaryContainer } from "./style";
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react';
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 
 export function Summary() {
+  const { transactions } = useContext(TransactionsContext)
+
+
   return (
     <SummaryContainer>
       <SummaryCard>
