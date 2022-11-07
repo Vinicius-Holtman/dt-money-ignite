c45f1406e84aaef0690bf06d899a892dd2cb1f24
Vinicius <viniholtman28@gmail.com>
2022-11-07 11:13:12 -0300

    Using hook memo

--- src/main.tsx
+++ src/main.tsx
@@ -7,3 +7,20 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <App />
   </React.StrictMode>
 )
+
+/*
+* Por que um componente renderiza?
+* - Hooks changed (mudou estado, contexto, reducer)
+* - Props changed (mudou propriedades)
+* - Parent rerendered (componente pai renderizou)
+*
+* Qual o fluxo de renderizacao?
+* 1. O React recria o HTML da interface daquele componente
+* 2. Compara a versao do HTML recriada com a versao anterior
+* 3. Se mudou alguma coisa, ele reecreve o HTML na tela
+* 
+* Memo:
+* 0.: Hook changed, Props changed (deep comparison)
+* 0.1: Comparar a versao anterior dos hooks e props
+* 0.2: Se mudou algo, ele vai permitir a nova renderizacao
+*/
