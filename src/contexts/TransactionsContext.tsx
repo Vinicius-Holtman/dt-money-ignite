15a47b25467cd929e313b750b30f13ac02efc765
Vinicius <viniholtman28@gmail.com>
2022-11-07 11:02:32 -0300

    Using useCallback

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useCallback, useEffect, useState } from "react";
 import { api } from "../lib/axios";
 import { createContext } from 'use-context-selector'
 
@@ -33,31 +33,37 @@ export const TransactionsContext = createContext({} as TransactionContextType)
 export function TransactionProvider({ children }: TransactionProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([])
 
-  async function createTransaction(data: CreateTransactionInput) {
-    const { description, price, category, type } = data;
+  const createTransaction = useCallback(
+    async (data: CreateTransactionInput) => {
+      const { description, price, category, type } = data;
+  
+      const response = await api.post('transactions', {
+        description,
+        price,
+        category,
+        type,
+        createdAt: new Date()
+      })
+  
+      setTransactions(state => [response.data, ...state])
+    },
+    []
+  )
 
-    const response = await api.post('transactions', {
-      description,
-      price,
-      category,
-      type,
-      createdAt: new Date()
-    })
-
-    setTransactions(state => [response.data, ...state])
-  }
-
-  async function fetchTransactions(query?: string) {
-    const response = await api.get('transactions', {
-      params: {
-        _sort: 'createdAt',
-        _order: 'desc',
-        q: query
-      }
-    })
-    
-    setTransactions(response.data)
-  }
+  const fetchTransactions = useCallback(
+    async (query?: string) => {
+      const response = await api.get('transactions', {
+        params: {
+          _sort: 'createdAt',
+          _order: 'desc',
+          q: query
+        }
+      })
+      
+      setTransactions(response.data)
+    },
+    []
+  )
 
   useEffect(() => {
     fetchTransactions()
